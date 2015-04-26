5c11393d53cd3032df52c6fc083af717324cd189
Antonio Campos <acamposruiz@gmail.com>
2015-04-26 08:53:58 +0200

    Import logic from angular-foursquare-api bower package

--- angular-googlemaps.js
+++ angular-googlemaps.js
@@ -2,9 +2,9 @@
  * Created by Antonio on 12/04/15.
  */
 angular.module('angular-googlemaps', [])
-    .factory('addressFromCoordinates', function($http, $q) {
+    .factory('addressFromCoordinates', function ($http, $q) {
         'use strict';
-        var addressFromCoordinates = function(latlng) {
+        var addressFromCoordinates = function (latlng) {
             var deferred = $q.defer();
             $http({
                 url: 'http://maps.googleapis.com/maps/api/geocode/json',
@@ -17,10 +17,36 @@ angular.module('angular-googlemaps', [])
                     'X-Requested-With': undefined
                 })
             }).
-                success(function(data){
+                success(function (data) {
                     deferred.resolve(data);
                 });
             return deferred.promise;
         };
         return addressFromCoordinates;
+    })
+    .factory('coordinatesFromAddress', function ($http, $q) {
+        'use strict';
+        var coordinatesFromAddress = function (address, section) {
+            var deferred = $q.defer();
+            $http({
+                url: 'http://maps.googleapis.com/maps/api/geocode/json',
+                params: {
+                    address: address,
+                    components: 'country:ES'
+                },
+                method: 'GET',
+                //data: data,
+                headers: angular.extend({
+                    'X-Requested-With': undefined
+                })
+            }).
+                success(function (data) {
+                    deferred.resolve({
+                        coordinates: data.results[0].geometry.location.lat + ',' + data.results[0].geometry.location.lng,
+                        section: section
+                    });
+                });
+            return deferred.promise;
+        };
+        return coordinatesFromAddress;
     })
